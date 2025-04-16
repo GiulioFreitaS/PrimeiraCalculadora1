ce02a11781d82663648b5d19e91206d10d07bb96
GiulioFreitaS <giudx1@gmail.com>
2025-04-16 15:10:09 -0300

    Add files via upload

---
+++ PrimeiraCalculadora1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiracalculadora1;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class PrimeiraCalculadora1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
